5071574dd092d4cbff5f1caeb218dbf1d6f90a24
Gabriel Hoffman Silva <gabrielhoffman@usp.br>
2019-12-07 01:00:51 -0300

    log actions

--- src/form.java
+++ src/form.java
@@ -1,14 +1,45 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class form {
     private JButton CREATEButton;
     private JButton READButton;
     private JButton UPDATEButton;
     private JButton DELETEButton;
+    private JPanel panel;
+    private static JFrame frame;
+
+    public form() {
+        actionListener(CREATEButton, "button create clicked");
+
+        actionListener(READButton, "button read clicked");
+
+        actionListener(UPDATEButton, "button update clicked");
+
+        actionListener(DELETEButton, "button delete clicked");
+    }
+
+    private void actionListener(JButton createButton, String s) {
+        createButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println(s);
+            }
+        });
+    }
 
     public static void main(String[] args) {
-        JFrame frame = new JFrame("Form");
-        frame.setContentPane(new Form().panelMain);
+        createFrame();
+        configureFrame();
+    }
+
+    private static void createFrame() {
+        frame = new JFrame("Form");
+    }
+
+    private static void configureFrame() {
+        frame.setContentPane(new form().panel);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
         frame.setVisible(true);
