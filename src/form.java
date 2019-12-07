dd74678d9833403d8c4086391d608187c791eaef
Gabriel Hoffman Silva <gabrielhoffman@usp.br>
2019-12-07 00:34:54 -0300

    adicionei main

--- src/form.java
+++ src/form.java
@@ -5,4 +5,12 @@ public class form {
     private JButton READButton;
     private JButton UPDATEButton;
     private JButton DELETEButton;
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Form");
+        frame.setContentPane(new Form().panelMain);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
 }
