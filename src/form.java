fd16f66161089b0d370ef50800730096d89b942f
Gabriel Hoffman Silva <gabrielhoffman@usp.br>
2019-12-07 00:28:03 -0300

    criar projeto base ui crud

---
+++ src/form.java
@@ -0,0 +1,8 @@
+import javax.swing.*;
+
+public class form {
+    private JButton CREATEButton;
+    private JButton READButton;
+    private JButton UPDATEButton;
+    private JButton DELETEButton;
+}
